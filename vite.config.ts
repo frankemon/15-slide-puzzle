aea8cd8f66115e4355afa0eb8f507ec8dc525c47
frankemon <frankie.hugo@hotmail.com>
2024-11-23 01:24:51 +0100

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  root: "./",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Specify the directory for static assets
+  },
+  base: "/projects/15-slide-puzzle/",
+  publicDir: "public",
+});
